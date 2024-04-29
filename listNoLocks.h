c432a3ac6e5ef2225c5e68dd1dab52a4b2bd1106
Pigf00d <thesolarpulse@gmail.com>
2024-04-28 22:05:59 -0400

    gottem

--- listNoLocks.h
+++ listNoLocks.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <stdatomic.h>
 
 
 typedef struct Node_lockless Node_lockless; 
@@ -20,7 +21,7 @@ struct Node_lockless
 typedef struct linked_list_lockless
 {
 	Node_lockless *head;
-	int size;
+	atomic_int size;
 } linked_list_lockless;
 
 static inline struct linked_list_lockless *
@@ -32,7 +33,7 @@ lll_create(void)
 		return NULL;
 	}
 	ll_ptr->head = NULL;
-	ll_ptr->size = 0;
+	atomic_init(&ll_ptr->size, 0);
 	return ll_ptr;
 }
 
@@ -67,7 +68,7 @@ lll_length(struct linked_list_lockless *ll)
 {
 	//should always give accurate response
 	if (ll == NULL) return 0;
-	return ll->size;
+	return atomic_load(&ll->size);
 }
 
 //need to support removal from anywhere in the list not just front
@@ -76,30 +77,35 @@ lll_remove(struct linked_list_lockless *ll, int key)
 {
 	
 	if (ll == NULL) return false;
-	if (ll->size == 0) return false; //moved lock below this
+	if (atomic_load(&ll->size) == 0) return false;
 	
 	Node_lockless *temp = ll->head;
-	if (temp->value == key && temp->deleted == false)
+	Node_lockless *prev = NULL;
+
+	while (temp != NULL && temp->deleted == true)
 	{
-		ll->size -= 1;
-		Node_lockless* prevtemp = temp;
-		temp = temp->next;
-		prevtemp->deleted = true;
-		free(prevtemp);
-		return true;
-	}
-	while (temp->next != NULL)
-	{
-		Node_lockless* toRemove = temp->next;
-		if (toRemove->value == key && toRemove->deleted == false)
+		if(temp->value == key && temp->deleted == false)
 		{
-			temp->next = toRemove->next;
-			toRemove->deleted = true;
-			ll->size -= 1;
-			free(toRemove);
-			return true;
+			if(prev == NULL) //removing head
+			{
+				if (__sync_bool_compare_and_swap(&ll->head, temp, temp->next)){
+					atomic_fetch_sub(&ll->size, 1);
+					temp->deleted = true;
+					free(temp);
+					return true;
+				}
+			}
+			else
+			{
+				if(__sync_bool_compare_and_swap(&prev->next, temp, temp->next))
+				{
+					atomic_fetch_sub(&ll->size, 1);
+					temp->deleted = true;
+					free(temp);
+					return true;
+				}
+			}
 		}
-		temp = temp->next;
 	}
 	//the value was not removed 
 	return false;
