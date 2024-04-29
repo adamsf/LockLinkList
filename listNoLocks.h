56721b43aea741b2d8b495239cbf62709f38a67c
Pigf00d <thesolarpulse@gmail.com>
2024-04-28 21:30:45 -0400

    this

--- listNoLocks.h
+++ listNoLocks.h
@@ -13,6 +13,7 @@ struct Node
 	int value;
 	Node_lockless *next;
 	bool deleted;
+	int ref_counter;
 };
 
 
@@ -20,23 +21,18 @@ typedef struct linked_list_lockless
 {
 	Node_lockless *head;
 	int size;
-	int ref_counter;
 } linked_list_lockless;
 
 static inline struct linked_list_lockless *
 lll_create(void)
 {
 	linked_list_lockless* ll_ptr = malloc(sizeof(linked_list_lockless));
-	ll_ptr->ref_counter = 1;
-	int cur_refs = ll_ptr->ref_counter;
-	while (cur_refs != ll_ptr->ref_counter); //wait until other threads have finished
 	if (ll_ptr == NULL)
 	{
 		return NULL;
 	}
 	ll_ptr->head = NULL;
 	ll_ptr->size = 0;
-	ll_ptr->ref_counter--;
 	return ll_ptr;
 }
 
@@ -44,15 +40,11 @@ static inline int
 lll_destroy(struct linked_list_lockless *ll)
 {
 	if (ll == NULL) return 0;
-	ll_ptr->ref_counter++;
-	int cur_refs = ll_ptr->ref_counter;
-	while (cur_refs != ll_ptr->ref_counter);
 	if (ll->size == 0)
 	{
 		free(ll);
 		return 1;
 	} 
-	ll_ptr->ref_counter--;
 	return 0;
 }
 
@@ -62,25 +54,20 @@ lll_add(struct linked_list_lockless *ll, int value)
 	
 	if (ll == NULL) return;
 	Node_lockless *temp = malloc(sizeof(Node)); //moved lock below this
-	ll->ref_counter++;
-	int cur_refs = ll->ref_counter;
-	while (cur_refs != ll->ref_counter);
+	temp->ref_counter = 1;
+	temp->deleted = false;
 	temp -> value = value;
     do {
         temp -> next = ll -> head;
     }while(__sync_bool_compare_and_swap(&ll -> head, temp -> next, temp) == 0);
-	ll->ref_counter--;
 }
 
 static inline int
 lll_length(struct linked_list *ll)
 {
 	//should always give accurate response
-	ll->ref_counter++;
-	int cur_refs = ll->ref_counter;
-	while (cur_refs != ll->ref_counter); //wait until other threads have finished
-	int sz = ll->size;
-	ll->ref_counter--;
+	if (ll == NULL) return 0;
+	retrun ll->size;
 }
 
 //need to support removal from anywhere in the list not just front
@@ -90,9 +77,7 @@ lll_remove(struct linked_list *ll, int key)
 	
 	if (ll == NULL) return false;
 	if (ll->size == 0) return false; //moved lock below this
-	ll->ref_counter++;
-	int cur_refs = ll->ref_counter;
-	while (cur_refs != ll->ref_counter);
+	
 	Node_lockless *temp = ll->head;
 	if (temp->value == key && temp->deleted == false)
 	{
@@ -117,7 +102,6 @@ lll_remove(struct linked_list *ll, int key)
 		temp = temp->next;
 	}
 	//the value was not removed 
-	ll->ref_counter--;
 	return false;
 }
 
@@ -128,9 +112,7 @@ lll_contains(struct linked_list_lockless *ll, int value)
 	
 	if (ll == NULL) return 0;
 	if (ll->size == 0) return 0; //moved lock below this
-	ll->ref_counter++;
-	int cur_refs = ll->ref_counter;
-	while (cur_refs != ll->ref_counter);
+	
 	Node_lockless* temp = ll->head;
 	int ctr = 1;
 	while (temp != NULL)
@@ -145,7 +127,6 @@ lll_contains(struct linked_list_lockless *ll, int value)
 			ctr++;
 		} 
 	}
-	ll->ref_counter--;
 	return 0;
 }
 
